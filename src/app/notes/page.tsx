b8c3548c28aa7802b2d7940e3a6d2b71831c0167
Henryle-hd <123258064+Henryle-hd@users.noreply.github.com>
2024-02-21 12:27:11 +0300

    noteEmpy

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 import prisma from "@/lib/db/prisma";
 import NodeList from "@/components/note-list";
+import { ArrowUpRight, BookPlus, Rabbit } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "easyAI-nodes",
@@ -14,10 +15,19 @@ export default async function NotePage() {
   const allNotes = await prisma.note.findMany({ where: { userId } });
 
   return (
-    <div className="grid grid-cols-1 gap-3 md:grid-cols-1 lg:grid-cols-3">
+    <div className="grid gap-3 sm:grid-cols-2  lg:grid-cols-3">
       {allNotes.map((note) => (
         <NodeList note={note} key={note.id} />
       ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full flex h-screen flex-col items-center justify-center gap-6">
+          <Rabbit size={200} strokeWidth={0.5} />
+          <div className="flex gap-3">
+            {"You don't have any notes yet. Try to add One "}
+            <ArrowUpRight color="orange" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
